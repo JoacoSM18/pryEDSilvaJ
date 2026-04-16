279e04b0a842e7643224b3bbe4e306086b939c53
JoacoSM18 <joako.sm.18@gmail.com>
2026-04-16 12:07:44 -0300

    En proceso

---
+++ pryEDSilvaJ/clsCola.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryEDSilvaJ
+{
+    internal class clsCola
+    {
+        //Campos de la Clase
+        private clsNodo pri;
+        private clsNodo ult;
+
+        //Propiedades de la Clase
+        public clsNodo Primero
+        {
+            get { return pri; }
+            set { pri = value; }
+        }
+        public clsNodo Ultimo
+        {
+            get { return ult; }
+            set { ult = value; }
+        }
+        //Metodos de la Clase
+        private void Agregar(clsNodo nuevo)
+        {
+            if (Primero == null)
+            {
+                Primero = nuevo;
+                Ultimo = nuevo;
+            }
+            else
+            {
+                Ultimo.Siguiente = nuevo;
+                Ultimo = nuevo;
+            }
+        }
+        public void Eliminar()
+        {
+            if (Primero == Ultimo)
+            {
+                Primero = null;
+                Ultimo = null;
+            }
+            else
+            {
+                Primero = Primero.Siguiente;
+            }
+        }
+
+    }
+}
