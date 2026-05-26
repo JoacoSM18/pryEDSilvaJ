a1ed0fb62c2447e4de0009761ec756b7992f0d0a
JoacoSM18 <joako.sm.18@gmail.com>
2026-05-26 11:08:51 -0300

    Cambios

---
+++ pryEDSilvaJ/clsGrafoMatricial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEDSilvaJ
+{
+    internal class clsGrafoMatricial
+    {
+        private String[] Ciudades = new string[] {"Córdoba", "Mendoza","Santa Fe","Buenos Aires","Salta"};
+        private Decimal[,] Precio = new Decimal[5, 5];
+        public void Agregar (Int32 f, Int32 c, Decimal p)
+        {
+            Precio[f, c] = p;
+        }
+        public void Eliminar (Int32 f, Int32 c)
+        {
+            Precio[f, c] = 0;
+        }
+        public Decimal Consultar (Int32 f, Int32 c)
+        {
+            return Precio[f, c];
+        }
+        public void BorrarTodo ()
+        {
+            for (Int32 f = 0; f < 5; f++)
+            {
+                for (Int32 c = 0; c < 5; c++)
+                {
+                    Precio[f, c] = 0;
+                }
+            }
+        }
+    }
+}
--- pryEDSilvaJ/frmGrafo.Designer.cs
+++ pryEDSilvaJ/frmGrafo.Designer.cs
@@ -28,30 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.grbCargaDatos = new System.Windows.Forms.GroupBox();
+            this.grbConsultaDatos = new System.Windows.Forms.GroupBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.grbListarViajes = new System.Windows.Forms.GroupBox();
+            this.dgvViajes = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grbCargaDatos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.grbListarViajes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvViajes)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // grbCargaDatos
             // 
-            this.groupBox1.Location = new System.Drawing.Point(247, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(264, 169);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.grbCargaDatos.Controls.Add(this.label2);
+            this.grbCargaDatos.Controls.Add(this.label1);
+            this.grbCargaDatos.Location = new System.Drawing.Point(247, 12);
+            this.grbCargaDatos.Name = "grbCargaDatos";
+            this.grbCargaDatos.Size = new System.Drawing.Size(264, 169);
+            this.grbCargaDatos.TabIndex = 0;
+            this.grbCargaDatos.TabStop = false;
+            this.grbCargaDatos.Text = "Carga de Datos";
             // 
-            // groupBox2
+            // grbConsultaDatos
             // 
-            this.groupBox2.Location = new System.Drawing.Point(526, 12);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(242, 169);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.grbConsultaDatos.Location = new System.Drawing.Point(526, 12);
+            this.grbConsultaDatos.Name = "grbConsultaDatos";
+            this.grbConsultaDatos.Size = new System.Drawing.Size(242, 169);
+            this.grbConsultaDatos.TabIndex = 1;
+            this.grbConsultaDatos.TabStop = false;
+            this.grbConsultaDatos.Text = "Consulta de Datos";
             // 
             // pictureBox1
             // 
@@ -61,37 +69,71 @@
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
-            // groupBox3
+            // grbListarViajes
             // 
-            this.groupBox3.Location = new System.Drawing.Point(12, 200);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(756, 184);
-            this.groupBox3.TabIndex = 3;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "groupBox3";
+            this.grbListarViajes.Controls.Add(this.dgvViajes);
+            this.grbListarViajes.Location = new System.Drawing.Point(12, 200);
+            this.grbListarViajes.Name = "grbListarViajes";
+            this.grbListarViajes.Size = new System.Drawing.Size(756, 360);
+            this.grbListarViajes.TabIndex = 3;
+            this.grbListarViajes.TabStop = false;
+            this.grbListarViajes.Text = "Listar Viajes";
+            // 
+            // dgvViajes
+            // 
+            this.dgvViajes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvViajes.Location = new System.Drawing.Point(14, 73);
+            this.dgvViajes.Name = "dgvViajes";
+            this.dgvViajes.Size = new System.Drawing.Size(729, 273);
+            this.dgvViajes.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
             // 
             // frmGrafo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(780, 410);
-            this.Controls.Add(this.groupBox3);
+            this.ClientSize = new System.Drawing.Size(784, 585);
+            this.Controls.Add(this.grbListarViajes);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grbConsultaDatos);
+            this.Controls.Add(this.grbCargaDatos);
             this.Name = "frmGrafo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Operaciones en un Grafo";
+            this.grbCargaDatos.ResumeLayout(false);
+            this.grbCargaDatos.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.grbListarViajes.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvViajes)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox grbCargaDatos;
+        private System.Windows.Forms.GroupBox grbConsultaDatos;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox grbListarViajes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvViajes;
     }
 }
