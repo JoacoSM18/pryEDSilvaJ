49eb8986fc9d58cc10f9870bec93f60dcd04daa2
JoacoSM18 <joako.sm.18@gmail.com>
2026-05-26 11:25:22 -0300

    Andando clase 26/05

--- pryEDSilvaJ/clsGrafoMatricial.cs
+++ pryEDSilvaJ/clsGrafoMatricial.cs
@@ -9,21 +9,21 @@ namespace pryEDSilvaJ
 {
     internal class clsGrafoMatricial
     {
-        private String[] Ciudades = new string[] {"Córdoba", "Mendoza","Santa Fe","Buenos Aires","Salta"};
+        private String[] Ciudades = new string[] { "Córdoba", "Mendoza", "Santa Fe", "Buenos Aires", "Salta" };
         private Decimal[,] Precio = new Decimal[5, 5];
-        public void Agregar (Int32 f, Int32 c, Decimal p)
+        public void Agregar(Int32 f, Int32 c, Decimal p)
         {
             Precio[f, c] = p;
         }
-        public void Eliminar (Int32 f, Int32 c)
+        public void Eliminar(Int32 f, Int32 c)
         {
             Precio[f, c] = 0;
         }
-        public Decimal Consultar (Int32 f, Int32 c)
+        public Decimal Consultar(Int32 f, Int32 c)
         {
             return Precio[f, c];
         }
-        public void BorrarTodo ()
+        public void BorrarTodo()
         {
             for (Int32 f = 0; f < 5; f++)
             {
@@ -31,6 +31,66 @@ namespace pryEDSilvaJ
                 {
                     Precio[f, c] = 0;
                 }
+            }
+        }
+        public void MostrarDestinos(Int32 f, DataGridView Grilla)
+        {
+            Grilla.Rows.Clear();
+            Grilla.Columns.Clear();
+            Grilla.Columns.Add("Col1", "Destino");
+            Grilla.Columns.Add("Col2", "Precio");
+            Grilla.Columns[0].Width = 200;
+            Grilla.Columns[1].Width = 200;
+            Grilla.Rows.Clear();
+            for (Int32 c = 0; c < 5; c++)
+            {
+                if (Precio[f, c] > 0)
+                {
+                    Grilla.Rows.Add(Ciudades[c], Precio[f, c]);
+                }
+            }
+        }
+        public void MostrarOrigenes(Int32 c, DataGridView Grilla)
+        {
+            Grilla.Rows.Clear();
+            Grilla.Columns.Clear();
+            Grilla.Columns.Add("Col1", "Origen");
+            Grilla.Columns.Add("Col2", "Precio");
+            Grilla.Columns[0].Width = 200;
+            Grilla.Columns[1].Width = 200;
+            for (Int32 f = 0; f < 5; f++)
+            {
+                if (Precio[f, c] > 0)
+                {
+                    Grilla.Rows.Add(Ciudades[f], Precio[f, c]);
+                }
+            }
+        }
+        public void MostrarTodo(DataGridView Grilla)
+        {
+            Grilla.Rows.Clear();
+            Grilla.Columns.Clear();
+
+            Grilla.Columns.Add("NomOrigen", "Origen");
+            for (Int32 i = 0; i < 5; i++)
+            {
+                Grilla.Columns.Add("Destino" + i.ToString(), Ciudades[i]);
+            }
+            for (Int32 f = 0; f < 5; f++)
+            {
+                Grilla.Rows.Add(Ciudades[f]);
+                for (Int32 c = 0; c < 5; c++)
+                {
+                    Grilla.Rows[f].Cells[c + 1].Value = Precio[f, c];
+                }
+            }
+        }
+        public void MostrarCiudades (ComboBox cmb)
+        {
+            cmb.Items.Clear();
+            for (Int32 i = 0; i < 5; i++)
+            {
+                cmb.Items.Add(Ciudades[i]);
             }
         }
     }
--- pryEDSilvaJ/frmGrafo.Designer.cs
+++ pryEDSilvaJ/frmGrafo.Designer.cs
@@ -33,9 +33,20 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.grbListarViajes = new System.Windows.Forms.GroupBox();
             this.dgvViajes = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblOrigen = new System.Windows.Forms.Label();
+            this.lblDestino = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblOrigen2 = new System.Windows.Forms.Label();
+            this.lblDestino2 = new System.Windows.Forms.Label();
+            this.lblPrecio2 = new System.Windows.Forms.Label();
+            this.lblDesde = new System.Windows.Forms.Label();
+            this.lblHasta = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.grbCargaDatos.SuspendLayout();
+            this.grbConsultaDatos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.grbListarViajes.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvViajes)).BeginInit();
@@ -43,8 +54,11 @@
             // 
             // grbCargaDatos
             // 
-            this.grbCargaDatos.Controls.Add(this.label2);
-            this.grbCargaDatos.Controls.Add(this.label1);
+            this.grbCargaDatos.Controls.Add(this.button2);
+            this.grbCargaDatos.Controls.Add(this.button1);
+            this.grbCargaDatos.Controls.Add(this.lblPrecio);
+            this.grbCargaDatos.Controls.Add(this.lblDestino);
+            this.grbCargaDatos.Controls.Add(this.lblOrigen);
             this.grbCargaDatos.Location = new System.Drawing.Point(247, 12);
             this.grbCargaDatos.Name = "grbCargaDatos";
             this.grbCargaDatos.Size = new System.Drawing.Size(264, 169);
@@ -54,6 +68,11 @@
             // 
             // grbConsultaDatos
             // 
+            this.grbConsultaDatos.Controls.Add(this.button4);
+            this.grbConsultaDatos.Controls.Add(this.button3);
+            this.grbConsultaDatos.Controls.Add(this.lblPrecio2);
+            this.grbConsultaDatos.Controls.Add(this.lblDestino2);
+            this.grbConsultaDatos.Controls.Add(this.lblOrigen2);
             this.grbConsultaDatos.Location = new System.Drawing.Point(526, 12);
             this.grbConsultaDatos.Name = "grbConsultaDatos";
             this.grbConsultaDatos.Size = new System.Drawing.Size(242, 169);
@@ -71,6 +90,8 @@
             // 
             // grbListarViajes
             // 
+            this.grbListarViajes.Controls.Add(this.lblHasta);
+            this.grbListarViajes.Controls.Add(this.lblDesde);
             this.grbListarViajes.Controls.Add(this.dgvViajes);
             this.grbListarViajes.Location = new System.Drawing.Point(12, 200);
             this.grbListarViajes.Name = "grbListarViajes";
@@ -87,23 +108,113 @@
             this.dgvViajes.Size = new System.Drawing.Size(729, 273);
             this.dgvViajes.TabIndex = 0;
             // 
-            // label1
+            // lblOrigen
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 39);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.lblOrigen.AutoSize = true;
+            this.lblOrigen.Location = new System.Drawing.Point(6, 27);
+            this.lblOrigen.Name = "lblOrigen";
+            this.lblOrigen.Size = new System.Drawing.Size(41, 13);
+            this.lblOrigen.TabIndex = 0;
+            this.lblOrigen.Text = "Origen:";
             // 
-            // label2
+            // lblDestino
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(46, 84);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.lblDestino.AutoSize = true;
+            this.lblDestino.Location = new System.Drawing.Point(6, 64);
+            this.lblDestino.Name = "lblDestino";
+            this.lblDestino.Size = new System.Drawing.Size(46, 13);
+            this.lblDestino.TabIndex = 1;
+            this.lblDestino.Text = "Destino:";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(7, 102);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(40, 13);
+            this.lblPrecio.TabIndex = 2;
+            this.lblPrecio.Text = "Precio:";
+            // 
+            // lblOrigen2
+            // 
+            this.lblOrigen2.AutoSize = true;
+            this.lblOrigen2.Location = new System.Drawing.Point(5, 27);
+            this.lblOrigen2.Name = "lblOrigen2";
+            this.lblOrigen2.Size = new System.Drawing.Size(41, 13);
+            this.lblOrigen2.TabIndex = 0;
+            this.lblOrigen2.Text = "Origen:";
+            // 
+            // lblDestino2
+            // 
+            this.lblDestino2.AutoSize = true;
+            this.lblDestino2.Location = new System.Drawing.Point(5, 64);
+            this.lblDestino2.Name = "lblDestino2";
+            this.lblDestino2.Size = new System.Drawing.Size(46, 13);
+            this.lblDestino2.TabIndex = 1;
+            this.lblDestino2.Text = "Destino:";
+            // 
+            // lblPrecio2
+            // 
+            this.lblPrecio2.AutoSize = true;
+            this.lblPrecio2.Location = new System.Drawing.Point(5, 102);
+            this.lblPrecio2.Name = "lblPrecio2";
+            this.lblPrecio2.Size = new System.Drawing.Size(40, 13);
+            this.lblPrecio2.TabIndex = 2;
+            this.lblPrecio2.Text = "Precio:";
+            // 
+            // lblDesde
+            // 
+            this.lblDesde.AutoSize = true;
+            this.lblDesde.Location = new System.Drawing.Point(11, 40);
+            this.lblDesde.Name = "lblDesde";
+            this.lblDesde.Size = new System.Drawing.Size(41, 13);
+            this.lblDesde.TabIndex = 1;
+            this.lblDesde.Text = "Desde:";
+            // 
+            // lblHasta
+            // 
+            this.lblHasta.AutoSize = true;
+            this.lblHasta.Location = new System.Drawing.Point(241, 40);
+            this.lblHasta.Name = "lblHasta";
+            this.lblHasta.Size = new System.Drawing.Size(38, 13);
+            this.lblHasta.TabIndex = 2;
+            this.lblHasta.Text = "Hasta:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(183, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(8, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(161, 140);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // frmGrafo
             // 
@@ -119,8 +230,11 @@
             this.Text = "Operaciones en un Grafo";
             this.grbCargaDatos.ResumeLayout(false);
             this.grbCargaDatos.PerformLayout();
+            this.grbConsultaDatos.ResumeLayout(false);
+            this.grbConsultaDatos.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.grbListarViajes.ResumeLayout(false);
+            this.grbListarViajes.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvViajes)).EndInit();
             this.ResumeLayout(false);
 
@@ -132,8 +246,18 @@
         private System.Windows.Forms.GroupBox grbConsultaDatos;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.GroupBox grbListarViajes;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDestino;
+        private System.Windows.Forms.Label lblOrigen;
         private System.Windows.Forms.DataGridView dgvViajes;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblPrecio2;
+        private System.Windows.Forms.Label lblDestino2;
+        private System.Windows.Forms.Label lblOrigen2;
+        private System.Windows.Forms.Label lblHasta;
+        private System.Windows.Forms.Label lblDesde;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
     }
 }
